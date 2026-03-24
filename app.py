fcf3a9b1458d66443c68692b06729765b43e270a
Moustapha9999 <sallmoustapha567@gmail.com>
2026-03-24 16:52:26 +0000

    Fixer le port

--- app.py
+++ app.py
@@ -86,7 +86,9 @@ def stats():
         'f1_score': 0.8288
     })
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+import os
 
-    
+if __name__ == "__main__":
+    # Render définit une variable d'environnement PORT
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)    
